9aa1e7f7df4fe8d9f8c7b0d80c6c3ce9c8868193
Dhruv-M-Sathavara <dhruvms1234@gmail.com>
2024-11-06 17:28:24 +0530

    Pattern extra Questions

--- patterns.cpp
+++ patterns.cpp
@@ -5,15 +5,80 @@ using namespace std;
 
 //Q1 
 
-int main(){
-int n  = 5;
-    for(int i=1 ; i<=5 ; i++){
-        for(int j=1 ; j<n ; j++){
-            cout<<j;
-        }
-        cout<<endl;
+// int main(){
+// int n  = 5;
+//     for(int i=1 ; i<=5 ; i++){
+//         for(int j=1 ; j<n ; j++){
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//     }
+
+//Q2
+
+    // int main(){
+    // int n  = 5;
+    // for(int i=1 ; i<n ; i++){
+    //     for(int j=1 ; j<n ; j++){
+    //         cout<<"*"<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+//Q3
+
+    // int main(){
+    // int n  = 5;
+    // for(int i=1 ; i<n ; i++){
+    //     char ch = 'A';
+    //     for(int j=1 ; j<n ; j++){
+    //         cout<<ch++<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+//Q4
+
+    // int main(){
+    // int n  = 4;
+    // int num = 1;
+    // for(int i=1 ; i<n ; i++){
+        
+    //     for(int j=1 ; j<n ; j++){
+    //         cout<<num++<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // cout << "After pattern : " << num << endl;
+
+    // int main(){
+    // int n  = 4;
+    // char ch = 'A';
+    // for(int i=1 ; i<n ; i++){
+        
+    //     for(int j=1 ; j<n ; j++){
+    //         cout<<ch++<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // cout << "After pattern : " << ch << endl;
+
+//Q5
+
+    int main(){
+
+        int n = 6;
+        
+        for(int i = 0 ; i<n ;i++){
+            for(int j = 0; j<i+1 ; j++){
+                cout<<(i+1);
+            }
+            cout<<endl;
+        }  
+
     }
 
-    
 
-}
+    
