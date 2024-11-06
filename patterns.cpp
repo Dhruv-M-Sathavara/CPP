deac058d7978f02e05f84381446367cc43efd086
Dhruv-M-Sathavara <dhruvms1234@gmail.com>
2024-11-06 13:53:19 +0530

    loop q

---
+++ patterns.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+
+//Q1 
+
+int main(){
+int n  = 5;
+    for(int i=1 ; i<=5 ; i++){
+        for(int j=1 ; j<n ; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+
+    
+
+}
