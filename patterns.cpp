0a345299b836dd3e06b4c38aafe80ab30bde3aa3
Dhruv-M-Sathavara <dhruvms1234@gmail.com>
2024-11-08 18:53:03 +0530

    pattern 2.0

--- patterns.cpp
+++ patterns.cpp
@@ -77,17 +77,166 @@ using namespace std;
     //         }
     //         cout<<endl;
     //     }  
-    int main(){
+    // int main(){
 
-        int n = 6;
+    //     int n = 6;
+        
+    //     for(int i = 0 ; i<n ;i++){
+    //         for(int j = 0; j<i+1 ; j++){
+    //             cout<<(i+1);
+    //         }
+    //         cout<<endl;
+    //     }  
+
+        // int main(){
+
+        // int n = 6;
+        
+        // for(int i = 0 ; i<n ;i++){
+            
+        //     for(int j = 1; j<i+1 ; j++){
+        //         cout<<(j);
+        //     }
+        //     cout<<endl;
+        // }  
+
+        //Q6
+
+        // int main(){
+
+        // int n = 6;
+        
+        // for(int i = 0 ; i<n ;i++){
+                
+        //     for(int j = (i+1); j>0 ; j--){
+        //         cout<<(j);
+        //     }
+        //     cout<<endl;
+        // } 
+
+        //Q7
+
+        // int main(){
+
+        // int n = 5;
+        // int count = 1;
+        
+        // for(int i = 0 ; i<n ;i++){
+            
+        //     for(int j = 1; j<i+1 ; j++){
+        //         cout<<(count++)<<" ";
+        //     }
+        //     cout<<endl;
+        // }  
+
+    //Q7
+
+    // int main(){
+
+    //     int n = 5;
+    //     char ch = 'A';
+        
+    //     for(int i = 0 ; i<n ;i++){
+            
+    //         for(int j = 1; j<i+1 ; j++){
+    //             cout<<(ch++)<<" ";
+    //         }
+    //         cout<<endl;
+    //     }  
+
+//Q8
+
+    // int main(){
+
+    //     int n = 5;
+    //     int count = 1;
+        
+    //     for(int i = 0 ; i<n ;i++){
+
+    //         for (int j = 0; j < i; j++)
+    //         {
+    //             cout<<" ";
+    //         }
+            
+            
+    //         for(int j = 0; j<n-i ; j++){
+    //             cout<<(i+1);
+    //         }
+    //         cout<<endl;
+    //     }  
+
+//  Q9
+    // int main(){
+
+    //     int n = 5;
+    //     int count = 1;
+        
+    //     for(int i = 0 ; i<n ;i++){
+
+    //         for (int j = 0; j < n - i; j++)
+    //         {
+    //             cout<<" ";
+    //         }
+            
+            
+    //         for(int j = 1; j< i+1 ; j++){
+    //             cout<<(j);
+    //         }
+
+    //         for(int j = i+1; j>0; j-- )
+    //         {
+    //             cout<<(j);
+    //         }
+            
+    //         cout<<endl;
+    //     }  
+
+//Q10
+
+  int main(){
+
+        int n = 5;
+        int count = 1;
         
         for(int i = 0 ; i<n ;i++){
-            for(int j = 0; j<i+1 ; j++){
-                cout<<(i+1);
+
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                cout<<" ";
+            }
+            cout << "*";
+            
+            if(i != 0){
+                for(int j=0 ; j <2*i -1 ; j++){
+                    cout<<" ";
+                }
+                cout<<"*";
             }
             cout<<endl;
-        }  
+            
+            }
+//bottom
+            for(int i =0; i<n-1 ;i++){
+
+                for(int j = 0 ;j<i+1;j++){
+                    cout<<" ";
+                }
+
+                cout<<"*";
+
+                if(i != n-2){
+
+                     for(int j = 0 ;j<2*(n-i)-5;j++){
+                    cout<<" ";
+
+                }
+                cout<<"*";
+            }
+
+          cout<<endl;
+        } 
     }
+  
 
 
     
