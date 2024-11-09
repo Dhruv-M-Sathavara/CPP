e56f2c59590270e4a48963f92d176c92d7870d82
Dhruv-M-Sathavara <dhruvms1234@gmail.com>
2024-11-09 14:42:20 +0530

    Pattern last

--- patterns.cpp
+++ patterns.cpp
@@ -193,50 +193,92 @@ using namespace std;
 
 //Q10
 
-  int main(){
+//   int main(){
 
-        int n = 5;
-        int count = 1;
+//         int n = 5;
+//         int count = 1;
+        
+//         for(int i = 0 ; i<n ;i++){
+
+//             for (int j = 0; j < n - i - 1; j++)
+//             {
+//                 cout<<" ";
+//             }
+//             cout << "*";
+            
+//             if(i != 0){
+//                 for(int j=0 ; j <2*i -1 ; j++){
+//                     cout<<" ";
+//                 }
+//                 cout<<"*";
+//             }
+//             cout<<endl;
+            
+//             }
+// //bottom
+//             for(int i =0; i<n-1 ;i++){
+
+//                 for(int j = 0 ;j<i+1;j++){
+//                     cout<<" ";
+//                 }
+
+//                 cout<<"*";
+
+//                 if(i != n-2){
+
+//                      for(int j = 0 ;j<2*(n-i)-5;j++){
+//                     cout<<" ";
+
+//                 }
+//                 cout<<"*";
+//             }
+
+//           cout<<endl;
+//         } 
+
+    int main(){
+
+        int n = 4;
+        
         
         for(int i = 0 ; i<n ;i++){
 
-            for (int j = 0; j < n - i - 1; j++)
-            {
-                cout<<" ";
-            }
-            cout << "*";
+           for (int j = 0 ; j<i+1 ; j++){
+            cout<<"*";
+
+           }
+
+           for(int j=0 ; j<2*n-2*i ; j++){
+            cout<<" ";
+           }
+           for(int j = 0;j<i+1;j++){
+            cout<<"*";
             
-            if(i != 0){
-                for(int j=0 ; j <2*i -1 ; j++){
-                    cout<<" ";
-                }
-                cout<<"*";
-            }
-            cout<<endl;
-            
-            }
-//bottom
-            for(int i =0; i<n-1 ;i++){
-
-                for(int j = 0 ;j<i+1;j++){
-                    cout<<" ";
-                }
-
-                cout<<"*";
-
-                if(i != n-2){
-
-                     for(int j = 0 ;j<2*(n-i)-5;j++){
-                    cout<<" ";
-
-                }
-                cout<<"*";
-            }
-
-          cout<<endl;
-        } 
+           }
+                
+        cout<<endl;
     }
-  
+
+     for(int i = n ; i>-1 ;i--){
+
+           for (int j = 0 ; j<i+1 ; j++){
+            cout<<"*";
+
+           }
+
+           for(int j=0 ; j<2*n-2*i ; j++){
+            cout<<" ";
+           }
+           for(int j = 0;j<i+1;j++){
+            cout<<"*";
+            
+           }
+                
+        cout<<endl;
+    }
+
+    
+}
 
 
     
