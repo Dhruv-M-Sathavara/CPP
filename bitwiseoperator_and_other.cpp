7de04717d5f13e55fc0210896a9f6e6f15ae0bdc
Dhruv-M-Sathavara <dhruvms1234@gmail.com>
2024-11-13 18:27:30 +0530

    Bitwise and Data Modifier

---
+++ bitwiseoperator_and_other.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+//Bitwise
+
+int main(){
+
+    // int a = 6 , b = 10;
+
+    // cout<<(a & b)<<endl;
+    // cout<<(a | b)<<endl;
+    // cout<<(a ^ b)<<endl;
+    // cout<<(10 << 2)<<endl;
+    // cout<<(10 >> 1)<<endl;
+
+//Data Type Modifier
+    unsigned int x = -10;
+
+    cout<<sizeof(int)<<endl;
+    cout<<sizeof(long int)<<endl; //it increse 4 bytes
+    cout<<x<<endl;
+
+    return 0;
+}
+
