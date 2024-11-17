da576897bcf4bc5fb88ddeb4622bb2a8c28465ed
Dhruv-M-Sathavara <dhruvms1234@gmail.com>
2024-11-17 18:55:23 +0530

    vector

---
+++ array/Vectors/vector_quetions.cpp
@@ -0,0 +1,71 @@
+//Q1 Sindle number
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+// int main(){
+
+//     vector<int>vec ={4,1,2,1,2};
+
+//     int ans = 0;
+
+//     for(int i : vec){
+
+//         ans = ans ^ i;
+
+//     }
+
+//     cout<<ans;
+
+
+// }
+
+//Q2 Linear Search
+
+
+// int main(){
+
+//     vector<int>vec = {1,2,3,4,69,35,8,9};
+//     int key = 8;
+
+
+//     for(int i =0; i<vec.size();i++){
+
+//         if(vec[i]==key){
+//             cout<<i;
+//         }
+//     }
+
+
+// }
+
+//Q3 Reaverse Vector
+
+int reverse (int size){
+    int start = 0;
+    int end = size - 1;
+
+     while(start<end){
+
+        swap(vec[start],vec[end]);
+        start++;
+        end--;
+
+    }
+    
+
+}
+
+int main(){
+
+    vector<int>vec = {1,2,3,4,69,35,8,9};
+    
+    int size = 8;
+
+    in
+
+    
+
+}
--- array/Vectors/vetor.cpp
+++ array/Vectors/vetor.cpp
@@ -1,9 +1,41 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
 int main(){
+    //veector<int> vec; for zise 0 empty vector
+    // vector<int> vec = {1,2,3}; //for any value
 
-    
+    // vector<int>vec(3,1);//for fixed value
+    // cout<<vec[0]<<endl;
+    // cout<<vec[1]<<endl;
+    // cout<<vec[2]<<endl;
+
+    //functions
+
+
+    vector<int> vec;
+
+    vec.push_back(7);
+    vec.push_back(17);
+    vec.push_back(27);
+    //vec.pop_back()
+   
+    cout<<"front = "<<vec.front()<<endl;
+    cout<<"back = "<<vec.back()<<endl;
+    cout<<"potision = "<<vec.at(0)<<endl;
+    cout<<"size = "<< vec.size()<<endl;
+    cout<<"Capacity = "<<vec.capacity()<<endl;
+
+    //using loop
+
+    for(int i : vec){
+
+        cout<<i<<endl;
+    }
+
+
+    return 0;
 
 }
