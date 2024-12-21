ee83d23b4395fb5abd94d9efb39a1facec8ad674
Dhruv-M-Sathavara <dhruvms1234@gmail.com>
2024-12-21 22:55:49 +0530

    nwestart

--- Practice/patterns.cpp
+++ Practice/patterns.cpp
@@ -3,89 +3,65 @@
 using namespace std;
 
 int main(){
-    int n = 5;
-    
+
+    // for(int i = 0; i<4;i++){
+    //     for(int j = 1;j<=4;j++){
+    //         cout << j;
+    //     }
+    //     cout<<endl;
+    // }
+    // char k ='A';
+    // for(int i = 0 ; i<3; i++){
+       
+    //     for( int j = 0 ; j<3; j++){
+    //         cout << k++;
+            
+    //     }
+    //     cout<<endl;
+    // }
+
     // for(int i = 0 ; i<4;i++){
-    //     for(int j = 0 ; j<4 ;j++){
-    //         cout<<"*";
-    //     }
-    //     cout<<endl;
-    // }
-    
-    // for (int i =0;i<4;i++){
-    //     int k = 0;
-    //     for(int j = 0 ; j<i+1 ;j++){
-    //         cout<<k+1;
+    //     for(int j = 0; j<i+1; j++){
+    //         cout<< i;
     //     }
     //     cout<<endl;
     // }
 
-    //  for (int i =0;i<4;i++){
-        
-    //     for(int j = 1 ; j<i+1 ;j++){
+    //  for(int i = 0 ; i<4;i++){
+    //     for(int j = 1; j<=i+1; j++){
+    //         cout<< j;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 
+    // int k = 1;
+    // for(int i =1 ;i<=4;i++){
+    //     for(int j = 1 ; j <=i; j++){
+    //         cout<<k++;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for(int i = 0 ;i<4 ;i++){
+    //     for(j = 1 ; j<4 ; j++){
     //         cout<<j;
     //     }
-    //     cout<<endl;
-    // }
+    //
+    //  }
 
-    //  for (int i =0;i<4;i++){
-        
-    //     for(int j = 1 ; j<i+1 ;j++){
-    //         cout<<i;
-    //     }
-    //     cout<<endl;
-    // }
-
-    // for(int i = 0 ; i<5 ; i++){
-    //     for(int j = 5; j>i ; j--){
-    //         cout<<"8";
-    //     }
-    //     cout<<endl;
-    // }
-    
-    // for(int i = 0 ; i<n ; i++){
-    //     for(int j = 1 ; j<(n-i) ; j++){
-    //         cout<<j;
-    //     }
-    //     cout<<endl;
-    // }
-
-    // for(int i = 0 ;i< n ;i++){
-    //     for(int j = n;j>i;j--){
-    //         cout<<" ";
-    //     }
-
-    //      for(int j = 0;j<=i;j++){
-    //         cout<<"*";
-    //     }
-
-    //    if(i>0){
-    //      for(int j = 0;j<i;j++){
-    //         cout<<"*";
-    //     }
-    //    }
-    //     cout<<endl;
-        
-    // }
-    
-     for(int i = 0 ;i< n ;i++){
-    
-        for(int j = 0;j<i;j++){
+    for(int i = 1;i<=4;i++){
+        for(int j = 4 ; j>i ; j--){
             cout<<" ";
         }
+        for(int j = 1; j<=i;j++){
+            cout<<j;
+        }
+        for(int j = i ; j>0 ;j--){
+                cout<<j;
+        }
         
-       
-        for(int j = 1;j<n-i;j++){
-            cout<<"*";
-        }
-
-         for(int j = n;j>i;j--){
-            cout<<"*";
-        }
-
-       
         cout<<endl;
-        
     }
 
 }
